3c8c1ee97f4c879430ed9b8fa37b40ba97a30ef8
hmppa <t5lojo00@students.oamk.fi>
2018-10-19 10:28:14 +0300

    huippu salainen 5

--- app/src/main/java/e/antti/igdbapp/ListActivity.java
+++ app/src/main/java/e/antti/igdbapp/ListActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,12 +23,16 @@ public class ListActivity extends android.app.ListActivity {
 
     ArrayList<HashMap<String,String>> data = new ArrayList<>();
 
+    private GameWrapper gameWrapper;
+
     private static final String TAG_NAME = "name";
     private static final String TAG_ID = "id";
     private static final String TAG_RATING = "rating";
     private static final String TAG_RATING_COUNT = "rating_count";
     private static final String TAG_AGGREGATED_RATING = "aggregated_rating";
     private static final String TAG_AGGREGATED_RATING_COUNT = "aggregated_rating_count";
+    private static final String TAG_URL = "url";
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +45,21 @@ public class ListActivity extends android.app.ListActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // getting values from selected ListItem
-                String name = ((TextView) view.findViewById(R.id.name)).getText().toString();
-                String argument1 = ((TextView) view.findViewById(R.id.argument1)).getText().toString();
-                String argument2 = ((TextView) view.findViewById(R.id.argument2)).getText().toString();
-                String argument3 = ((TextView) view.findViewById(R.id.argument3)).getText().toString();
-                String argument4 = ((TextView) view.findViewById(R.id.argument4)).getText().toString();
+//                String name = ((TextView) view.findViewById(R.id.name)).getText().toString();
+//                String argument1 = ((TextView) view.findViewById(R.id.argument1)).getText().toString();
+//                String argument2 = ((TextView) view.findViewById(R.id.argument2)).getText().toString();
+//                String argument3 = ((TextView) view.findViewById(R.id.argument3)).getText().toString();
+//                String argument4 = ((TextView) view.findViewById(R.id.argument4)).getText().toString();
 
                 // Starting new intent
                 Intent in = new Intent(getApplicationContext(), Main2Activity.class);
-                in.putExtra(TAG_NAME, name);
-                in.putExtra(TAG_RATING, argument1);
-                in.putExtra(TAG_RATING_COUNT, argument2);
-                in.putExtra(TAG_AGGREGATED_RATING, argument3);
-                in.putExtra(TAG_AGGREGATED_RATING_COUNT, argument4);
+                in.putExtra("game",gameWrapper );
+                in.putExtra(TAG_ID, id);
+
+
+
+                Toast toast = Toast.makeText(getApplicationContext(),"" +id, Toast.LENGTH_SHORT);
+                toast.show();
 
                 startActivity(in);
 
@@ -60,7 +67,7 @@ public class ListActivity extends android.app.ListActivity {
         });
 
 
-        GameWrapper gameWrapper = (GameWrapper) getIntent().getSerializableExtra("game");
+        gameWrapper = (GameWrapper) getIntent().getSerializableExtra("game");
 
         ArrayList<Game> gameArrayList = gameWrapper.getItemDetails();
 
--- app/src/main/java/e/antti/igdbapp/Main2Activity.java
+++ app/src/main/java/e/antti/igdbapp/Main2Activity.java
@@ -3,7 +3,12 @@ package e.antti.igdbapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -20,11 +25,11 @@ public class Main2Activity extends AppCompatActivity {
     TextView criticScore;
     TextView criticScoreCount;
 
-    private String gName;
-    private String uScore;
-    private String uScoreCount;
-    private String cScore;
-    private String cScoreCount;
+//    private String gName;
+//    private String uScore;
+//    private String uScoreCount;
+//    private String cScore;
+//    private String cScoreCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +41,23 @@ public class Main2Activity extends AppCompatActivity {
         userScoreCount = (TextView) findViewById(R.id.userScoreCount);
         criticScore = (TextView) findViewById(R.id.criticScore);
         criticScoreCount = (TextView) findViewById(R.id.criticScoreCount);
+        ImageView imageView = findViewById(R.id.image);
 
         Intent intent = getIntent();
-        gName = intent.getStringExtra(TAG_NAME);
-        uScore = intent.getStringExtra(TAG_RATING);
-        uScoreCount = intent.getStringExtra(TAG_RATING_COUNT);
-        cScore = intent.getStringExtra(TAG_AGGREGATED_RATING);
-        cScoreCount = intent.getStringExtra(TAG_AGGREGATED_RATING_COUNT);
+        GameWrapper gameWrapper = (GameWrapper) getIntent().getSerializableExtra("game");
+        long id = intent.getLongExtra(TAG_ID, 0);
 
-        gameName.append(gName);
-        userScore.append(uScore);
-        userScoreCount.append(uScoreCount);
-        criticScore.append(cScore);
-        criticScoreCount.append(cScoreCount);
+        ArrayList<Game> gameArrayList = gameWrapper.getItemDetails();
+
+        Game game = gameArrayList.get((int) id);
+
+        gameName.append(game.getgName());
+        userScore.append(game.getuScore());
+        userScoreCount.append(game.getuScoreCount());
+        criticScore.append(game.getScore());
+        criticScoreCount.append(game.getScoreCount());
+
+        Picasso.get().load(game.getUrl()).resize(800, 500).into(imageView);
 
 
     }
