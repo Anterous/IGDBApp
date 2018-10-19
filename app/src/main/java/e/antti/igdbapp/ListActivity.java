5616e36a7e1c645e09cf2ce97f7fd3ab60205aa0
Veli Jakkula <t5jave00@students.oamk.fi>
2018-10-19 09:36:49 +0300

    single item OnClickListener that leads to non-existant activity

--- app/src/main/java/e/antti/igdbapp/ListActivity.java
+++ app/src/main/java/e/antti/igdbapp/ListActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,6 +34,31 @@ public class ListActivity extends android.app.ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         listView = getListView();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String name = ((TextView) view.findViewById(R.id.name)).getText().toString();
+                String argument1 = ((TextView) view.findViewById(R.id.argument1)).getText().toString();
+                String argument2 = ((TextView) view.findViewById(R.id.argument2)).getText().toString();
+                String argument3 = ((TextView) view.findViewById(R.id.argument3)).getText().toString();
+                String argument4 = ((TextView) view.findViewById(R.id.argument4)).getText().toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(), MainActivity.class);//TODO change activity 2 singleitemactivity lol
+                in.putExtra(TAG_NAME, name);
+                in.putExtra(TAG_RATING, argument1);
+                in.putExtra(TAG_RATING_COUNT, argument2);
+                in.putExtra(TAG_AGGREGATED_RATING, argument3);
+                in.putExtra(TAG_AGGREGATED_RATING_COUNT, argument4);
+
+                startActivity(in);
+
+            }
+        });
+
 
         GameWrapper gameWrapper = (GameWrapper) getIntent().getSerializableExtra("game");
 
